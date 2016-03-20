5db21f53b8b818fd9777eaee95d4f716f71d5c1b
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2016-03-20 00:52:21 -0600

    Add setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+import iou
+from os import path
+from pip.req import parse_requirements
+
+here = path.abspath(path.dirname(__file__))
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except:
+    with open(path.join(here, 'README.md')) as f:
+        long_description = f.read()
+
+install_requires = [str(ir.req)
+                    for ir in parse_requirements(
+                        path.join(here, 'requirements.txt'),
+                        session=False)]
+
+setup(
+    name=iou.__title__,
+    version=iou.__version__,
+    description=iou.__summary__,
+    long_description=long_description,
+    license=iou.__license__,
+    url=iou.__uri__,
+    author=iou.__author__,
+    author_email=iou.__email__,
+    packages=["iou", "iou.data"],
+    entry_points={
+        "console_scripts": [
+            "iou = iou.run:main",
+        ],
+    },
+    install_requires=install_requires,
+
+    # dependency_links=[
+    # "git+ssh://git@github.com/kdheepak89/click.git@7.0#egg=click-7.0"
+    # ]
+)
