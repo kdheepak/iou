05e921a85318e93400703b381ccc33342651ae6a
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2016-03-20 01:14:17 -0600

    Update minimize.py

--- minimize.py
+++ minimize.py
@@ -1,34 +1,36 @@
 """
 Minimize transactions between friends
 """
-import getpass
 import json
+import networkx as nx
 
-from coopr.pyomo import *
+from pyomo.environ import SolverFactory, Objective, ConcreteModel, RangeSet, Var, NonNegativeReals, Boolean, Constraint
 from oauth2client.client import SignedJwtAssertionCredentials
 import gspread
 
 
 def sign_in_gspread():
 
-    user_name = getpass.getuser()
-
     json_key = json.load(open(".iou"))
     scope = ['https://spreadsheets.google.com/feeds']
 
-    credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'].encode(), scope)
+    credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key[
+                                                'private_key'].encode(), scope)
 
     gc = gspread.authorize(credentials)
 
     return gc
 
+
 def create_graph(gc, expense_sheet_name):
 
     worksheet = gc.open(expense_sheet_name).sheet1
 
-    ### Find list of people involved
+    # Find list of people involved
     values_list = worksheet.row_values(1)
     people = values_list[3:]
+
+    G = nx.Graph()
 
     for person_name in people:
         if person_name is None:
@@ -42,37 +44,40 @@ def create_graph(gc, expense_sheet_name):
         number_of_contributors = sum([float(item) for item in row[3:]])
         for i, item in enumerate(row[3:]):
             if item != '0':
-                G.add_edge(people[i], row[2], owe=float(item)*float(row[1].strip('$'))/number_of_contributors)
+                G.add_edge(people[i], row[2], owe=float(
+                    item) * float(row[1].strip('$')) / number_of_contributors)
         print("{number_of_contributors} people owe {person} {amount} for {expense}".format(
-                number_of_contributors=number_of_contributors, 
-                person=row[2],
-                expense=row[0],
-                amount=row[1])
-             )
+            number_of_contributors=number_of_contributors,
+            person=row[2],
+            expense=row[0],
+            amount=row[1])
+        )
 
     return G
 
+
 def compress_edges(G):
-    ### empty dictionary
+    # empty dictionary
     net_value = {}
     for node in G.nodes():
         net_value[node] = 0
-
 
     for edge in G.edges(data=True):
         net_value[edge[0]] -= edge[2]['owe']
         net_value[edge[1]] += edge[2]['owe']
 
-
     for person, value in net_value.items():
         if value > 0:
-            print("{person}'s net spend \t{value}".format(person=person, value=-1*value))
+            print("{person}'s net spend \t{value}".format(
+                person=person, value=-1 * value))
         if value < 0:
-            print("{person}'s net spend \t {value}".format(person=person, value=-1*value))
+            print("{person}'s net spend \t {value}".format(
+                person=person, value=-1 * value))
 
     net_value = {key: -value for key, value in net_value.items()}
 
     return net_value
+
 
 def find_optimal(net_value):
 
@@ -85,37 +90,36 @@ def find_optimal(net_value):
     model.Lenders = RangeSet(1, len(lenders))
 
     model.Amount = Var(model.Loaners, model.Lenders, domain=NonNegativeReals)
-    model.TransactionsSwitch = Var(model.Loaners, model.Lenders, domain=Boolean)
+    model.TransactionsSwitch = Var(
+        model.Loaners, model.Lenders, domain=Boolean)
 
     def obj_expression(model):
-        return sum(model.Amount[i,j]*model.Amount[i,j] for i in model.Loaners for j in model.Lenders)
+        return sum(model.Amount[i, j] * model.Amount[i, j] for i in model.Loaners for j in model.Lenders)
 
     model.OBJ = Objective(rule=obj_expression)
 
     def loaner_constraint_rule(model, i):
-        return sum(model.Amount[i,j] for j in model.Lenders) <= net_value[loaners[i-1]]
+        return sum(model.Amount[i, j] for j in model.Lenders) <= net_value[loaners[i - 1]]
 
     def lender_constraint_rule(model, j):
-        return sum(model.Amount[i,j] for i in model.Loaners) <= -net_value[lenders[j-1]]
+        return sum(model.Amount[i, j] for i in model.Loaners) <= -net_value[lenders[j - 1]]
 
-    model.AmountConstraintLoaner = Constraint(model.Loaners, rule=loaner_constraint_rule)
-    model.AmountConstraintLender = Constraint(model.Lenders, rule=lender_constraint_rule)
+    model.AmountConstraintLoaner = Constraint(
+        model.Loaners, rule=loaner_constraint_rule)
+    model.AmountConstraintLender = Constraint(
+        model.Lenders, rule=lender_constraint_rule)
 
-    #model.pprint()
-
-    import coopr.environ
-    from coopr.opt.base import SolverFactory
-
+    # model.pprint()
 
     opt = SolverFactory('ipopt')
-     
+
     # create model instance, solve
     instance = model.create()
     results = opt.solve(instance)
-    instance.load(results) #load results back into model framework
-     
+    instance.load(results)  # load results back into model framework
+
     for i in range(0, len(loaners)):
         for j in range(0, len(lenders)):
-            if(instance.Amount[i+1, j+1].value != 0.0):
-                print("{} pays {} a total of ${:.2f}".format(loaners[i], lenders[j], (instance.Amount[i+1, j+1].value)))
-
+            if(instance.Amount[i + 1, j + 1].value != 0.0):
+                print("{} pays {} a total of ${:.2f}".format(
+                    loaners[i], lenders[j], (instance.Amount[i + 1, j + 1].value)))
